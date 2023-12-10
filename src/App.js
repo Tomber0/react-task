a83996bdca56ab6be2f06eea7fcabd1f180160cf
Aleksey.Zaitsau <aleseyzaicev@gmail.com>
2023-12-10 23:01:31 +0300

    fixed remote origin error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const TermsOfUse = ( { onAccept }) => {
   const[termsList, setTerms] = useState([]);
 
   useEffect(()=>{
     async function getTerms(){
-      const response = await fetch('http://167.71.69.158/static/test.json');
+      const response = await fetch('http://localhost:8080/static/json/JSON_DATA.json');
       const data = await response.json();
       setTerms(data.terms_of_use.paragraphs);
     }
@@ -28,15 +28,21 @@ const TermsOfUse = ( { onAccept }) => {
   );
 };
 
+
+
+
 const MainContent = () => {
   const [imageList, setImages] = useState([]);
   useEffect(() => {
-    async function getTerms() {
-      const response = await fetch('http://167.71.69.158/static/test.json');
+    async function getImages() {
+      const response = await fetch('http://localhost:8080/static/json/JSON_DATA.json');
       const data = await response.json();
+      data.images.map((img,index)=>{
+        img.image_url = 'http://localhost:8080' + img.image_url;
+      });
       setImages(data.images);
     }
-    getTerms();
+    getImages();
   }, []);
 
   const saveImage = (image) => {
@@ -57,6 +63,72 @@ const MainContent = () => {
     </div>
   );
 };
+const ImageCollection = () => {
+  const canvasRefs = useRef([]);
+
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/static/json/JSON_DATA.json');
+        const data = await response.json();
+        let img_id = 0;
+        data.images.map((img, index) => {
+          img.id = img_id++;
+          img.image_url = 'http://localhost:8080' + img.image_url;
+
+        });
+        setImages(data.images);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  const saveImage = (image) => {
+    const canvas = canvasRefs.current[image.id];
+    const link = document.createElement('a');
+    link.download = `image_${image.id}.png`;
+    link.href = canvas.toDataURL();
+    link.click();
+  };
+
+  return (
+    <div>
+      {images.map((image) => (
+        <div key={image.id}>
+          <canvas crossOrigin="anonymous" ref={(el) => (canvasRefs.current[image.id] = el)} />
+          <button onClick={() => saveImage(image)}>Save Image</button>
+          {image && (
+            <img
+              src={image.image_url}
+              alt={`ig ${image.id}`}
+              style={{ display: 'none' }}
+              onLoad={() => {
+                const canvas = canvasRefs.current[image.id];
+                const context = canvas.getContext('2d');
+                const img = new Image();
+                img.src = image.image_url;
+                img.onload = () => {
+                  img.crossOrigin = "anonymous";
+                  canvas.width = img.width;
+                  canvas.height = img.height;
+                  context.drawImage(img, 0, 0);
+                };
+
+              }}
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
 
 const App = () => {
   const [acceptedTerms, setAcceptedTerms] = useState(false);
@@ -70,7 +142,7 @@ const App = () => {
       {!acceptedTerms ? (
         <TermsOfUse onAccept={handleAcceptTerms} />
       ) : (
-        <MainContent />
+          <ImageCollection />
       )}
     </div>
   );
